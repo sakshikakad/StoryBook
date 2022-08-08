57bc0ebc78981232b3ac19c195a08207dd7b31cc
sakshikakad <64691121+sakshikakad@users.noreply.github.com>
2022-08-08 09:55:59 +0530

    remainig work done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
@@ -19,13 +20,43 @@ connectDB()
 
 const app = express()
 
+//body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+//Method Override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+    }
+}))
+
 //logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+//Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
 //handlbars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine(
+    '.hbs',
+    exphbs.engine({
+        helpers: {
+            formatDate,
+            truncate,
+            stripTags,
+            editIcon,
+            select,
+        },
+        defaultLayout: 'main',
+        extname: '.hbs',
+    })
+)
 app.set('view engine', '.hbs')
 
 //Sessions
@@ -45,13 +76,19 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Set global var
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
+
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
-
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 //@desc login/Landing page
 //@route Get/
 router.get('/', ensureGuest, function (req, res, next) {
@@ -12,7 +14,18 @@ router.get('/', ensureGuest, function (req, res, next) {
 
 //@desc dashboard
 //@route Get/dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('/error/500')
+
+    }
+
 })
 module.exports = router
